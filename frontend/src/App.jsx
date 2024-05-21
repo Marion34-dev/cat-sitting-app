cbfe787393d44c67a176c90dd3d9be510a58a00c
Marion Jung <marionjung34@hotmail.com>
2024-05-21 16:06:22 +0100

    React application setup with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// contains the main React component, i.e. landing page.
+
+import LandingPage from './components/LandingPage.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App
